f751b19c6b8f3762d9a0f5886da94d0edf255d8d
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-01-08 14:50:35 -0300

    Configuração inicial do projeto
    - Adicionando a rota de login e registro do usuário;
    - Configuração adicionar do Swagger para Documentação da API;

---
+++ CRUD_usuario/CRUD_usuario/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using CRUD_usuario.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_usuario.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        //criando minha primeira rota
+        [HttpPost]
+        [Route("logar")]
+        public IActionResult Logar(LoginViewModelinput loginViewModelInput)
+        {
+            return Ok(loginViewModelInput);
+        }
+
+        [Route("registrar")]
+        public IActionResult Registrar(RegistroViewModelInput registroViewModelInput)
+        {
+            return Created("", registroViewModelInput);
+        }
+    }
+}
---
+++ CRUD_usuario/CRUD_usuario/Model/RegistroViewModelInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_usuario.Model
+{
+    public class RegistroViewModelInput
+    {
+        [Required(ErrorMessage = "O Login é obrigatório")]
+        public string Login { get; set;}
+        [Required(ErrorMessage = "O Email é obrigatório")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "A Senha é obrigatória")]
+        public string Senha { get; set; }
+    }
+}
